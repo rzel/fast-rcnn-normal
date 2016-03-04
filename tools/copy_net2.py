02c8f97261f286e4924016561968fb7e59be8157
xiaolonw <xiaolonw@cs.cmu.edu>
2016-03-03 22:55:44 -0500

    cp

--- tools/copy_net2.py
+++ tools/copy_net2.py
@@ -11,14 +11,14 @@ import numpy as np
 
 
 caffe.set_mode_cpu()
-net = caffe.Net('/nfs.yoda/xiaolonw/fast_rcnn/fast-rcnn-normal/scripts/pre_gang/train.prototxt', caffe.TRAIN)
+net = caffe.Net('/nfs.yoda/xiaolonw/fast_rcnn/fast-rcnn-normal/scripts/dcgan_norm/train.prototxt', caffe.TRAIN)
 headfile = '/nfs.yoda/xiaolonw/torch_projects/weights2/head_r.txt'
 f = open(headfile, 'r')
 
-savename = '/nfs.yoda/xiaolonw/fast_rcnn/models/pre_gang/fast_rcnn_zero.caffemodel'
+savename = '/nfs.yoda/xiaolonw/fast_rcnn/models/dcgan_norm/fast_rcnn_zero.caffemodel'
 
-layer_num = 5
-layernames = ('da_conv1', 'da_conv2', 'da_conv3', 'da_conv4', 'da_conv5')
+layer_num = 4
+layernames = ('da_conv1', 'da_conv2', 'da_conv3', 'da_conv4')
 
 
 for i in xrange(layer_num):
